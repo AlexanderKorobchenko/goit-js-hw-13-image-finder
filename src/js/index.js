01235a1681753c02514cf4812fa85c56d7f355c0
AlexanderKorobchenko <korobchenko_ua@ukr.net>
2021-08-14 20:00:38 +0300

    third push
    -кнопка вверх
    - добавлены класси
    - считает кол-во картинок
    - сообщение, если картинка последняя

--- src/js/index.js
+++ src/js/index.js
@@ -1,84 +1,86 @@
 // импорт
 var _ = require('lodash');// lodash.debounce
 import cards from '../templates/card.hbs';// handlebars
-import { error } from '../../node_modules/@pnotify/core/dist/PNotify.js';// pnotify
+import { error, info } from '../../node_modules/@pnotify/core/dist/PNotify.js';// pnotify
 import * as basicLightbox from 'basiclightbox';//basicLightbox
+import PictureAPIServise from './apiService';
+import Loader from './loader.js';
+
+const newPictureAPIServise = new PictureAPIServise();
+const changeLoader = new Loader('.loader');
 
 // DOM-элементы
 const refs = {
     formEl: document.getElementById('search-form'),
     listEl: document.getElementById('gallery'),
-    loader: document.querySelector('.loader')
-}
-
-//options
-let pageNumber = 1;
-let inputValue = '';
-const options = {
-    baseURL: 'https://pixabay.com/api/',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    page: pageNumber,
-    per_page: '12',
-    key: '22892994-722ced5920981906a643cad7c'
+    endContetn: document.querySelector('.gallery__end')
 }
 
 // слушатели
-refs.formEl.addEventListener('input', _.debounce(onSearch, 800));
-window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-        //console.log('scroll, page:', pageNumber)
-        createLoader();
-        getData2(options, inputValue, pageNumber);
-    }
-});
+refs.formEl.addEventListener('input', _.debounce(onInputSearch, 800));
+window.addEventListener('scroll', onScpollSearch);
 
-// передача значения инпута
-function onSearch(event) {
+// вызов действия по реакции инпута
+function onInputSearch(event) {
     event.preventDefault();
 
-    if (inputValue !== event.target.value) {
-        inputValue = event.target.value;
-        refs.listEl.innerHTML = '';
-        pageNumber = 1;
+    refs.endContetn.classList.add('hidden');// прячем "End content" если он открыт
+    newPictureAPIServise.resetAmount();//обнуляем сумму загруженных картинок
+
+    // ввод пустой?
+    if (event.target.value === '') {
+        clearCards();// очищаем страницу
+        newPictureAPIServise.resetPage();// номер страницы - 0
+        return;
     };
 
-    if (inputValue === '') {
-        refs.listEl.innerHTML = '';
-        pageNumber = 1;
-        return
-    };
-
-    createLoader()
-    getData(options, inputValue);
+    // все в порядке, работаем...
+    changeLoader.addLoader();// загрузчик +
+    clearCards();// очищаем страницу
+    newPictureAPIServise.value = event.target.value;//передаем значение инпута
+    newPictureAPIServise.resetPage();// номер страницы - 0
+    newPictureAPIServise.getData().then(result => {
+        processResults(result);// обработка результата fetch
+        changeLoader.clearLoader();// закргузчик -
+        info({
+            text: `Total images found: ${newPictureAPIServise.total}`,
+            delay: 2000,
+        });// сколько найдено
+    });
 };
 
-function getData({ baseURL, image_type, orientation, page, per_page, key }, value) {
-    fetch(`${baseURL}?image_type=${image_type}&orientation=${orientation}&q=${value}&page=${page}&per_page=${per_page}&key=${key}`)
-        .then(response => response.json())
-        .then(result => processResults(result))
-        .catch(error => console.warn(error));
-}
+// вызов действия по реакции скролла
+function onScpollSearch() {
+    // доскролил до конца?
+    if (document.documentElement.scrollTop + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        // картинки уже закончились?
+        if (newPictureAPIServise.amount >= newPictureAPIServise.total) {
+            refs.endContetn.classList.remove('hidden');
+            return;
+        };
+        // все в порядке, работаем...
+        changeLoader.addLoader();// закргузчик +
+        newPictureAPIServise.getData().then(result => {
+            processResults(result);// обработка результата fetch
+            changeLoader.clearLoader();// закргузчик -
+        });
+    }
+};
 
-function getData2({ baseURL, image_type, orientation, per_page, key }, value, page) {
-    fetch(`${baseURL}?image_type=${image_type}&orientation=${orientation}&q=${value}&page=${page}&per_page=${per_page}&key=${key}`)
-        .then(response => response.json())
-        .then(result => processResults(result))
-        .catch(error => console.warn(error));
-}
-
-// обработка результата: проверка на ошибку, рендер, прослушка для модалки
+// обработка результата: проверка на ошибку, рендер, модалкa
 function processResults(result) {
     // ошибка, если ничего не найдено
     if (result.hits.length === 0) {
-        error({ text: `${inputValue} not found!` });
-        refs.listEl.innerHTML = '';
+        error({
+            text: `No results were found for "${newPictureAPIServise.inputValue}".`,
+            //mode: 'dark'
+        });
+        clearCards();
         return;
     };
 
     // рендерим карточки
     makeCards(result.hits);
-    pageNumber++;
 
     // слушаем карточки
     refs.listEl.addEventListener('click', onFindElement);
@@ -89,27 +91,25 @@ function processResults(result) {
         const photoById = getPhotoById(result.hits, id);
         makeModalWindow(photoById);
     };
-}
+};
 
-// создаем карточки, используя handlebars
+// создаем карточки (handlebars)
 function makeCards(array) {
     refs.listEl.insertAdjacentHTML('beforeend', cards(array));
-}
+};
+
+//удаляем карточки
+function clearCards() {
+    refs.listEl.innerHTML = '';
+};
 
 // поиск элемента по ID
 function getPhotoById(array, id) {
     return array.find(element => element.id === +id);
-}
+};
 
 // модалка basicLightbox
 function makeModalWindow(object) {
-    const instance = basicLightbox.create(`<img src='${object.largeImageURL}' width="800" height="600">`);
+    const instance = basicLightbox.create(`<img src='${object.largeImageURL}'>`);
     instance.show();
 };
-
-function createLoader() {
-    refs.loader.classList.remove('hidden');
-    setTimeout(() => {
-        refs.loader.classList.add('hidden');
-    }, 2000);
-}
